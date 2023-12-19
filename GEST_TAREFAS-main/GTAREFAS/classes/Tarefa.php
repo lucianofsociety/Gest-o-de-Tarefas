3a7103113a03f44ae2a7ac5899eea4ab64df33b8
Luciano Albuquerque <106563026+lucianofsociety@users.noreply.github.com>
2023-12-19 09:40:46 -0300

    inicio16/12/2023

---
+++ GEST_TAREFAS-main/GTAREFAS/classes/Tarefa.php
@@ -0,0 +1,61 @@
+<?php
+class Tarefa {
+    private $conn;
+
+
+    function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+    public function read()
+    {
+        $query = "SELECT * FROM  tbtarefa";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        
+        return $stmt;
+    }
+
+    public function readEdits($id)
+    {
+        $query = "SELECT id FROM tbtarefa WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function readEd($id)
+    {
+        $query = "SELECT * FROM tbtarefa WHERE fk_idusu = :fk_idusu";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":fk_idusu", $id);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function update($id, $tarefa, $data )
+    {
+        $query = "UPDATE tbtarefa SET tarefa = ?, data = ? WHERE id = ?";
+      
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$tarefa, $data, $id]);
+        return $stmt;
+    }
+
+    public function delete($id) {
+        $query = "DELETE FROM tbtarefa WHERE id = ?"; 
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$id]);
+        return $stmt;
+    }
+
+    public function create($tarefa, $data, $fk_idusu)
+    {
+        $query = "INSERT INTO  tbtarefa (tarefa,  data , fk_idusu) VALUES (?, ?, ?)";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$tarefa, $data, $fk_idusu]);
+        return $stmt;
+    }
+
+}
---
+++ GEST_TAREFAS-main/GTAREFAS/classes/user.php
@@ -0,0 +1,107 @@
+<?php
+class User {
+    private $conn;
+
+
+    function __construct($conn) {
+        $this->conn = $conn;
+    }
+
+
+    public function register($username, $password, $confirm_password, $sexo) {
+        // Verifique se as senhas coincidem
+        if ($password !== $confirm_password) {
+            echo "As senhas não coincidem. Tente novamente.";
+            return;
+        }
+
+
+        try {
+            // Hash da senha (substitua esta linha pela sua lógica de hash)
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+
+            // Insira no banco de dados
+            $stmt = $this->conn->prepare("INSERT INTO tbuser (username, password, sexo) VALUES (:username, :password, :sexo)");
+            $stmt->bindParam(':username', $username);
+            $stmt->bindParam(':password', $hashed_password);
+            $stmt->bindParam(':sexo', $sexo);
+            $stmt->execute();
+
+
+            echo "Registro bem-sucedido!";
+        } catch (PDOException $e) {
+            echo "Erro no registro: " . $e->getMessage();
+        }
+    }
+    
+
+
+    public function login($username, $password) {
+        try {
+            $stmt = $this->conn->prepare("SELECT * FROM tbuser WHERE username = :username");
+            $stmt->bindParam(':username', $username);
+            $stmt->execute();
+            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+
+            if ($user && password_verify($password, $user['password'])) {
+                // Login bem-sucedido
+                session_start();
+                $_SESSION['username'] = $user['username'];
+                $_SESSION['id'] = $user['id'];
+                $_SESSION['adm'] = $user['adm'];
+                return true;
+            } else {
+                // Credenciais inválidas
+                return false;
+            }
+        } catch (PDOException $e) {
+            echo "Erro no login: " . $e->getMessage();
+        }
+    }
+
+
+    public function logout() {
+        try {
+            session_start();
+            session_destroy();
+        } catch (PDOException $e) {
+            echo "Erro no logout: " . $e->getMessage();
+        }
+    }
+
+    public function delete($id) {
+        $query = "DELETE FROM tbuser WHERE id = ?"; 
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$id]);
+        return $stmt;
+    }
+
+    public function update($id, $username, $sexo )
+    {
+        $query = "UPDATE tbuser SET username = ?, sexo = ? WHERE id = ?";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute([$username, $sexo, $id]);
+        return $stmt;
+    }
+
+    public function read()
+    {
+        $query = "SELECT * FROM  tbuser";
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        
+        return $stmt;
+    }
+
+    public function readEdits($id)
+    {
+        $query = "SELECT id FROM tbuser WHERE id = :id";
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        return $stmt;
+    }
+}
+?>
